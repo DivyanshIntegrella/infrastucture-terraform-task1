6261ec5f57f358db9585d5bdd7c6b2e464a157ac
DivyanshIntegrella <divyansh.jain@integrella.com>
2023-05-03 12:05:57 +0530

    Application gateway and routing done.

--- main.tf
+++ main.tf
@@ -19,21 +19,22 @@ resource "azurerm_subnet" "subnet1" {
 
 resource "azurerm_subnet" "subnet2" {
     address_prefixes = ["10.0.2.0/24"]
-    name = "Appgw-Subnet"
+    name = "AppGw-Subnet"
     virtual_network_name = azurerm_virtual_network.azure-terraform.name
     resource_group_name = "rg-divansh-playground"
 }
 
-resource "azurerm_dns_zone" "azure-terraform" {
-  name = "DNS-test.com"
-  resource_group_name = "rg-divansh-playground"
-}
+# resource "azurerm_dns_zone" "azure-terraform" {
+#   name = "DNS-test.com"
+#   resource_group_name = "rg-divansh-playground"
+# }
 
 resource "azurerm_public_ip" "terraform" {
   name = "pip-test"
   resource_group_name = "rg-divansh-playground"
   location = "West Europe"
   allocation_method = "Static"
+  sku = "Standard"
 }
 
 resource "azurerm_network_interface" "main" {
@@ -43,7 +44,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name = "testip1"
-    subnet_id = azurerm_subnet.subnet2.id
+    subnet_id = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
   }
 }
@@ -83,3 +84,118 @@ resource "azurerm_virtual_machine" "main" {
   delete_data_disks_on_termination = true
 }
 
+locals {
+  backend_address_pool_name      = "gateway-beap"
+  frontend_port_name             = "gateway-feport"
+  frontend_ip_configuration_name = "gateway-feip"
+  http_setting_name              = "gateway-be-htst"
+  listener_name                  = "gateway-httplstn"
+  request_routing_rule_name      = "gateway-rqrt"
+  redirect_configuration_name    = "gateway-rdrcfg"
+}
+
+resource "azurerm_application_gateway" "gateway_1" {
+  name = "Appgate-agw"
+  resource_group_name = "rg-divansh-playground"
+  location = "West Europe"
+
+  sku {
+    name = "Standard_v2"
+    tier = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name = "gateway-ip-configuration"
+    subnet_id = azurerm_subnet.subnet2.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.terraform.id
+  }
+
+   backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+   backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    # path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+   http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+    priority = 1
+  }
+}
+
+resource "azurerm_virtual_network" "azure-terraform_2" {
+  name = "Hub-Vnet"
+  location = "West Europe"
+  resource_group_name = "rg-divansh-playground"
+  address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet3" {
+    address_prefixes = ["10.0.2.0/24"]
+    name = "AzureFirewallSubnet"
+    virtual_network_name = azurerm_virtual_network.azure-terraform_2.name
+    resource_group_name = "rg-divansh-playground"
+}
+
+resource "azurerm_public_ip" "firewall_ip" {
+  name                = "firewall_testpip"
+  location            = "West Europe"
+  resource_group_name = "rg-divansh-playground"
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "firewall1" {
+  name                = "testfirewall"
+  location            = "West Europe"
+  resource_group_name = "rg-divansh-playground"
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "firwall_configuration"
+    subnet_id            = azurerm_subnet.subnet3.id
+    public_ip_address_id = azurerm_public_ip.firewall_ip.id
+  }
+}
+
+resource "azurerm_route_table" "route1" {
+  name                = "acceptanceTestRouteTable1"
+  location            = "West Europe"
+  resource_group_name = "rg-divansh-playground"
+}
+
+resource "azurerm_route" "route1" {
+  name                = "acceptanceTestRoute1"
+  resource_group_name = "rg-divansh-playground"
+  route_table_name    = azurerm_route_table.route1.name
+  address_prefix      = "10.1.0.0/16"
+  next_hop_type       = "VnetLocal"
+}
