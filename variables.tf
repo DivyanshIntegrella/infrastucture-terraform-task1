529a97bf359d73f01e78225b164c1f4301945e00
DivyanshIntegrella <divyansh.jain@integrella.com>
2023-05-18 12:59:04 +0530

    trying -var command

--- variables.tf
+++ variables.tf
@@ -5,23 +5,13 @@ variable "rg_name" {
 }
 
 variable "azure-terraform"{
-    type = string
-    default = "App-Vnet"
-    description = "Virtual network"
 }
 
 variable "password" {
-  default = "Divyansh@123"
-  description = "password for virtual machine"
-  sensitive = true
 }
 
 variable "locals-name" {
-  type = string
-  default = "gateway"
 }
 
 variable "tenant" {
-  default = "bd1024c1-001a-4eeb-963a-cfeccbc90226"
-  sensitive = true
 }
